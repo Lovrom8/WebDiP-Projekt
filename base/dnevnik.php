c871c347bfe0a9fec244da59834468d4fb4bbc82
Lovro <lovro.posaric@gmail.com>
2021-04-25 12:49:17 +0200

    Dovršen log

--- base/dnevnik.php
+++ base/dnevnik.php
@@ -1,21 +1,19 @@
 <?php
 
 class Dnevnik {
-    private $baza = null;
-
-    static function dodajZapis($opis, $akcija, $korisnik)
+    static function dodajZapis($akcijaId, $opis, $korisnikId)
     {
         $baza = new Baza();
         $datum = date("Y-m-d H:i:s");
-        $upit = "INSERT INTO dnevnik VALUES(DEFAULT, '$opis')";
+        $upit = "INSERT INTO Dnevnik (ID_dnevnik, Opis, Datum_vrijeme, ID_korisnik, ID_tip_akcije) VALUES(DEFAULT, '$opis', '$datum', '$korisnikId', '$akcijaId')";
         $baza->provedi($upit);
     }
 
     static function dohvatiSveZapise()
     {
         $baza = new Baza();
-
-        
+        $upit = "SELECT * FROM Dnevnik D JOIN TipAkcije T ON T.ID_tip_akcije = D.ID_tip_akcije";
+        return $baza->dohvati($upit);
     }
 }
 
