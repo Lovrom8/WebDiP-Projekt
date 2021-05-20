486675783bb95d7e3399b6eb0b3d149d1c8104d4
Lovro <lovro.posaric@gmail.com>
2021-05-20 16:50:49 +0200

    Dodani privatni htaccess dio

--- privatno/korisnici.php
+++ privatno/korisnici.php
@@ -3,12 +3,10 @@ include_once '../base/baza.php';
 
 $baza = new Baza();
 
-$sql = "SELECT * FROM Korisnik K JOIN Uloga U ON U.D_uloga=K.ID_uloga";
-$stmt = $conn->prepare($sql); 
-$stmt->execute();
-$result = $stmt->get_result();
-while ($row = $result->fetch_assoc()) {
-    echo $row['Korisnicko ime'] ." ". $row["Lozinka"]." ".$row["NazivUloge"];
+$sql = "SELECT * FROM Korisnik K JOIN Uloga U ON U.ID_uloga=K.ID_uloga";
+$rezultat = $baza->dohvati($sql);
+while ($red = $rezultat->fetch_assoc()) {
+    echo $red['Korisnicko_ime'] ." ". $red["Lozinka"]." ".$red["Naziv_uloge"].'<br>';
 }
 
 ?>
